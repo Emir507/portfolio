9951550bf8696b12209eae71c85b5eb522bebdbc
Emir <dzhetybaev19emir@gmail.com>
2020-10-06 22:54:57 +0600

    deploy app

---
+++ precache-manifest.dcbe450a47b7681191eb4fb9e07953f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca04c0adf70771aa37fe5ea1beb18184",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "82ffca7d3619b71c3605",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8f5c6e708481bf742cef",
+    "url": "/portfolio/static/css/main.26d96f4e.chunk.css"
+  },
+  {
+    "revision": "82ffca7d3619b71c3605",
+    "url": "/portfolio/static/js/2.79aa56e5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.79aa56e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5c6e708481bf742cef",
+    "url": "/portfolio/static/js/main.080eb39a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
